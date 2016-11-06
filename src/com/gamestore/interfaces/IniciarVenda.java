157f19d0d587ef1cbd40283a7e53aae7aec69019
roger <roger@DESKTOP-VV307JB>
2016-11-06 17:31:40 -0200

    Branch Services
    - Servico cliente 90%
    - Servico produto 90%
    - Servico vendas 90%
    - Servico relatorio 0%

--- src/com/gamestore/interfaces/IniciarVenda.java
+++ src/com/gamestore/interfaces/IniciarVenda.java
@@ -105,6 +105,11 @@ public class IniciarVenda extends javax.swing.JPanel {
         });
 
         botaoIncluirCliente.setText(">>");
+        botaoIncluirCliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoIncluirClienteActionPerformed(evt);
+            }
+        });
 
         jLabel8.setFont(new java.awt.Font("Calibri Light", 0, 14)); // NOI18N
         jLabel8.setText("PRODUTO");
@@ -117,6 +122,11 @@ public class IniciarVenda extends javax.swing.JPanel {
         });
 
         botaoIncluirProduto.setText(">>");
+        botaoIncluirProduto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoIncluirProdutoActionPerformed(evt);
+            }
+        });
 
         tabelaProdutos.setFont(new java.awt.Font("Calibri Light", 0, 14)); // NOI18N
         tabelaProdutos.setModel(new javax.swing.table.DefaultTableModel(
@@ -190,7 +200,7 @@ public class IniciarVenda extends javax.swing.JPanel {
                         .addComponent(labelIdadeClienteSelecionado))
                     .addComponent(labelEmailClienteSelecionado)
                     .addComponent(labelFidelidadeClienteSelecionado))
-                .addContainerGap(269, Short.MAX_VALUE))
+                .addContainerGap(259, Short.MAX_VALUE))
         );
         piClienteLayout.setVerticalGroup(
             piClienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -227,7 +237,7 @@ public class IniciarVenda extends javax.swing.JPanel {
         psCliente.setLayout(psClienteLayout);
         psClienteLayout.setHorizontalGroup(
             psClienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(scrollPaneClientes, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 614, Short.MAX_VALUE)
+            .addComponent(scrollPaneClientes, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 604, Short.MAX_VALUE)
         );
         psClienteLayout.setVerticalGroup(
             psClienteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -260,25 +270,28 @@ public class IniciarVenda extends javax.swing.JPanel {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(28, 28, 28)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel8)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(scrollPaneProdutos)
-                                    .addComponent(textProduto))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(botaoIncluirProduto))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel3)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(panelCliente, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(textCliente))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(botaoIncluirCliente))))
+                            .addComponent(jLabel8, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(textProduto)
+                            .addComponent(textCliente))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(botaoIncluirCliente)
+                            .addComponent(botaoIncluirProduto)))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(labelTotal)))
+                        .addComponent(labelTotal))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(90, 90, 90)
+                                .addComponent(panelCliente, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(90, 90, 90)
+                                .addComponent(scrollPaneProdutos)))
+                        .addGap(58, 58, 58)))
                 .addContainerGap())
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(botaoCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -374,22 +387,9 @@ public class IniciarVenda extends javax.swing.JPanel {
 
     private void listaProdutosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_listaProdutosMouseClicked
         
-        try
-        {
-            DefaultListModel model = (DefaultListModel) listaProdutos.getModel();        
-            String selectedProduto = (String)model.getElementAt(listaProdutos.getSelectedIndex());
-
-            int selectedProdutoId = Integer.parseInt(selectedProduto.substring(0, selectedProduto.indexOf("-")).trim());
-
-            incluirNovoProduto(servicoProduto.obterItemPorId(selectedProdutoId));
-        }
-        catch(Exception ex)
-        {
-            JOptionPane.showMessageDialog(this, "Não foi possível incluir o produto.");
-        }        
     }//GEN-LAST:event_listaProdutosMouseClicked
 
-    private void incluirNovoProduto(Produto produto){
+    private void incluirNovoProduto(Produto produto) throws Exception {
         if (produto == null){
             JOptionPane.showMessageDialog(this, "Produto não encontrado.");
             return;
@@ -397,36 +397,19 @@ public class IniciarVenda extends javax.swing.JPanel {
             
         DefaultTableModel model = (DefaultTableModel) tabelaProdutos.getModel();
         
+        servico.adicionarItem(produto, 1);
+        
         Object[] row = new Object[5];
-                row[0] = produto.getId();
-                row[1] = produto.getNome();
-                row[2] = produto.getPreco();
-                row[3] = 1;
-                row[4] = 1 * produto.getPreco();
-                model.addRow(row);                
+        row[0] = produto.getId();
+        row[1] = produto.getNome();
+        row[2] = produto.getPreco();
+        row[3] = 1;
+        row[4] = 1 * produto.getPreco();
+        model.addRow(row);                
     }
     
     private void listaClientesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_listaClientesMouseClicked
-        try
-        {
-            DefaultListModel model = (DefaultListModel) listaClientes.getModel();        
-            String selectedCliente = (String)model.getElementAt(listaClientes.getSelectedIndex());
 
-            int selectedId = Integer.parseInt(selectedCliente.substring(0, selectedCliente.indexOf("-")).trim());
-
-            Cliente cliente = servicoCliente.obterItemPorId(selectedId);
-            
-            labelNomeClienteSelecionado.setText(cliente.getNome());
-            labelIdadeClienteSelecionado.setText(cliente.getIdade());
-            labelEmailClienteSelecionado.setText(cliente.getEmail());
-            labelFidelidadeClienteSelecionado.setText("Cliente está comprando pela primeira vez");
-            
-            controlarPainelCliente("card2");
-        }
-        catch(Exception ex)
-        {
-            JOptionPane.showMessageDialog(this, "Não foi possível incluir o cliente.");
-        }     
     }//GEN-LAST:event_listaClientesMouseClicked
 
     private void textClienteKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_textClienteKeyReleased
@@ -455,6 +438,49 @@ public class IniciarVenda extends javax.swing.JPanel {
         revalidate();
     }//GEN-LAST:event_textClienteKeyReleased
 
+    private void botaoIncluirClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoIncluirClienteActionPerformed
+        try
+        {
+            DefaultListModel model = (DefaultListModel) listaClientes.getModel();        
+            String selectedCliente = (String)model.getElementAt(listaClientes.getSelectedIndex());
+
+            int selectedId = Integer.parseInt(selectedCliente.substring(0, selectedCliente.indexOf("-")).trim());
+
+            Cliente cliente = servicoCliente.obterItemPorId(selectedId);
+            
+            labelNomeClienteSelecionado.setText(cliente.getNome());
+            labelIdadeClienteSelecionado.setText(cliente.getIdade());
+            labelEmailClienteSelecionado.setText(cliente.getEmail());
+            labelFidelidadeClienteSelecionado.setText("Cliente está comprando pela primeira vez");
+            
+            servico.novoPedido(cliente);            
+            
+            controlarPainelCliente("card2");
+        }
+        catch(Exception ex)
+        {
+            JOptionPane.showMessageDialog(this, "Não foi possível incluir o cliente.");
+        }     
+    }//GEN-LAST:event_botaoIncluirClienteActionPerformed
+
+    private void botaoIncluirProdutoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoIncluirProdutoActionPerformed
+        try
+        {
+            DefaultListModel model = (DefaultListModel) listaProdutos.getModel();        
+            String selectedProduto = (String)model.getElementAt(listaProdutos.getSelectedIndex());
+
+            int selectedProdutoId = Integer.parseInt(selectedProduto.substring(0, selectedProduto.indexOf("-")).trim());
+
+            incluirNovoProduto(servicoProduto.obterItemPorId(selectedProdutoId));
+            scrollPaneProdutos.setVisible(false);
+            textProduto.setText("");
+        }
+        catch(Exception ex)
+        {
+            JOptionPane.showMessageDialog(this, "Não foi possível incluir o produto.");
+        }        
+    }//GEN-LAST:event_botaoIncluirProdutoActionPerformed
+
     private void controlarPainelCliente(String painel){
         CardLayout card = (CardLayout)panelCliente.getLayout();
         card.show(panelCliente, painel);
@@ -467,9 +493,9 @@ public class IniciarVenda extends javax.swing.JPanel {
         labelIdadeClienteSelecionado.setText("");
         labelNomeClienteSelecionado.setText("");
         labelTotal.setText("");
-        listaProdutos = new JList<>();        
         textCliente.setText("");
         textProduto.setText("");
+        panelCliente.setVisible(false);
                 
         DefaultTableModel model = (DefaultTableModel) tabelaProdutos.getModel();
         model.setRowCount(0);
